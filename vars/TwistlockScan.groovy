373ef55f5cbeace2aed4d67ef962dad7e85bb89d
bmarsh9 <bmarshall735@gmail.com>
2020-11-19 21:13:27 +0000

    edited groovy

--- vars/TwistlockScan.groovy
+++ vars/TwistlockScan.groovy
@@ -1,17 +1,16 @@
-//import org.example.Constants
-
 def call(Map options) {
-                def imageName = options.get("imageName",false)
-                def outputFile = options.get("outputFile","prisma-cloud-scan-results.json")
-                prismaCloudScanImage ca: '',
-                cert: '',
-                dockerAddress: 'unix:///var/run/docker.sock',
-                image: imageName,
-                key: '',
-                logLevel: 'debug',
-                podmanPath: '',
-                project: '',
-                resultsFile: outputFile,
-                ignoreImageBuildTime:true
+    def imageName = options.get("imageName",false)
+    def outputFile = options.get("outputFile","prisma-cloud-scan-results.json")
+
+    prismaCloudScanImage ca: '',
+      cert: '',
+      dockerAddress: 'unix:///var/run/docker.sock',
+      image: imageName,
+      key: '',
+      logLevel: 'debug',
+      podmanPath: '',
+      project: '',
+      resultsFile: outputFile,
+      ignoreImageBuildTime:true
 }
 
